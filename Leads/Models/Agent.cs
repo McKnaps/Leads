34a9a1570597783aff3f0c2c5cec347a8503a94c
Mikołaj <mikolaj.knapik02@gmail.com>
2025-01-24 16:04:01 +0100

    Added Sieve to Models

--- Leads/Models/Agent.cs
+++ Leads/Models/Agent.cs
@@ -1,11 +1,18 @@
+using Sieve.Attributes;
+
 namespace Leads.Models;
 
 public class Agent
 {
+    [Sieve(CanFilter = true, CanSort = true)]
     public int AgentId { get; set; }
+    [Sieve(CanFilter = true, CanSort = true)]
     public string FirstName { get; set; }
+    [Sieve(CanFilter = true, CanSort = true)]
     public string LastName { get; set; }
+    [Sieve(CanFilter = true, CanSort = true)]
     public string Email { get; set; }
+    [Sieve(CanFilter = true, CanSort = true)]
     public string OperatingArea { get; set; }
     public ICollection<Lead> Leads { get; set; }
     public string UserId { get; set; }
--- Leads/Models/Lead.cs
+++ Leads/Models/Lead.cs
@@ -1,14 +1,21 @@
+using Sieve.Attributes;
+
 namespace Leads.Models;
 
 public class Lead
 {
     public int LeadId { get; set; }
-    public PropertyType PropertyType { get; set; } 
+    [Sieve(CanFilter = true, CanSort = true)]
+    public PropertyType PropertyType { get; set; }
+    [Sieve(CanFilter = true, CanSort = true)]
     public string Location { get; set; }
+    [Sieve(CanFilter = true, CanSort = true)]
     public decimal Price { get; set; }
+    [Sieve(CanFilter = true, CanSort = true)]
     public string OwnerContact { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime ExpiresAt { get; set; }
+    [Sieve(CanFilter = true, CanSort = true)]
     public LeadStatus Status { get; set; } 
     public string RejectionReason { get; set; }
     public int AgentId { get; set; }
